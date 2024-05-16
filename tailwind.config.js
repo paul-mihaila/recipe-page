707d5c37a08d93b879b7a8a4571b07afc2781824
Paul Mihaila <paulmihaila98@gmail.com>
2024-05-16 17:33:46 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export const content = ['./*.html'];
+export const theme = {
+  screens: {
+    sm: '375px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  extend: {
+    colors: {
+      roseWhite: 'hsl(330, 100%, 98%)',
+      eggShell: 'hsl(30, 54%, 90%)',
+      lightGrey: 'hsl(30, 18%, 87%)',
+      wengeBrown: 'hsl(30, 10%, 34%)',
+      darkCharcoal: 'hsl(24, 5%, 18%)',
+      nutMeg: 'hsl(14, 45%, 36%)',
+      darkRaspberry: 'hsl(332, 51%, 32%)',
+    },
+  },
+};
+export const plugins = [];
